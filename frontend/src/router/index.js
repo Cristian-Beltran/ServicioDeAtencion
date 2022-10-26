693c96f4ff841f9a1364173e9ead6bcb22d0d63b
Cristian <cristian.beltran.paco@gmail.com>
2022-10-26 02:03:38 -0400

    Creacion de una nueva ruta en vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const routes = [
   {
@@ -10,11 +10,12 @@ const routes = [
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () => import("@/views/AboutView.vue"),
+  },
+  {
+    path: "/foot",
+    name: "foot",
+    component: () => import("@/views/FootView.vue"),
   },
 ];
 
